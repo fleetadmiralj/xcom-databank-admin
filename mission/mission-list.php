41a2a8e6191493f029181c0b786e019baa81a0ef
Joshua Chambers <joshua.e.chambers@gmail.com>
2021-10-31 21:29:54 -0400

    Additional style updates

--- mission/mission-list.php
+++ mission/mission-list.php
@@ -5,7 +5,7 @@
 <?php include_once $_SERVER['DOCUMENT_ROOT'].'/php/header-include.php' ?>
 <?php include_once $_SERVER['DOCUMENT_ROOT'].'/php/page-head.php' ?>
 			<div id="main">
-				<h2>List of All Mission</h2>
+				<h2 class="list-header">List of All Missions</h2>
 				<p><a href="/mission/mission.php">Add new Mission</a></p>
 				<?php listMissions(); ?>
 			</div>
--- soldier/mission-soldiers-list.php
+++ soldier/mission-soldiers-list.php
@@ -5,7 +5,7 @@
 <?php include_once $_SERVER['DOCUMENT_ROOT'].'/php/header-include.php' ?>
 <?php include_once $_SERVER['DOCUMENT_ROOT'].'/php/page-head.php' ?>
 			<div id="main">
-				<h2>List of All Mission Soldiers</h2>
+				<h2 class="list-header">List of Mission Soldiers</h2>
 				<p><a href="/soldier/mission-soldiers.php">Add new Mission Soldier</a></p>
 				<?php listMissionSoldier(); ?>
 			</div>
