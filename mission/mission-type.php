abc926bf7bac1e97dbfb5cd274ed2f22768c7056
Joshua Chambers <joshua.e.chambers@gmail.com>
2021-11-01 10:15:57 -0400

    Updating Mission Type Stuff

--- mission/mission-type.php
+++ mission/mission-type.php
@@ -1,33 +1,30 @@
 <?php include_once '/home/joshch9/project/adminInclude.php' ?>
 <?php
 
-	$missionType = new MissionType();
-	
+	$errorMsg = "";
 	if(!empty($_POST)) {
-		$missionTypeData = $_POST;
-		
-		if(isset($_POST['id'])) {
-			if(is_numeric($_POST['id'])) {
-				$missionType->editMissionType($missionTypeData);
-			}
-		}
-		else {
-			$missionType->newMissionType($missionTypeData);
-		}
-		createMissionJson();
-		header('Location: /mission/mission-type-list.php');
+		$errorMsg = missionTypeProcess($_POST, '/mission/mission-type-list.php');
 	}
 	else {
 		if(isset($_GET['id']) and is_numeric($_GET['id'])) {
 			$missionTypeID = $_GET['id'];
 			$missionType->getMissionType($missionTypeID);
-		} ?>
+		}
+	}		
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 <?php include_once $_SERVER['DOCUMENT_ROOT'].'/php/header-include.php' ?>
 <?php include_once $_SERVER['DOCUMENT_ROOT'].'/php/page-head.php' ?>
 			<div id="main" class="controls input-group">
+			<?php
+				if($errorMsg != "") {
+			?>
+				<p class="text-danger"><strong><?php echo $errorMsg; ?></strong></p>
+			<?php
+				}
+			?>
 				<form action="mission-type.php" method="post" id="mission-type-form">
 					<?php missionTypeForm($missionType); ?>
 					
