8a1263466c13a7a0c4cdc9287592de2060665978
Joshua Chambers <joshua.e.chambers@gmail.com>
2021-11-01 08:07:11 -0400

    Some additional style changes

--- mission/mission-type-list.php
+++ mission/mission-type-list.php
@@ -5,7 +5,7 @@
 <?php include_once $_SERVER['DOCUMENT_ROOT'].'/php/header-include.php' ?>
 <?php include_once $_SERVER['DOCUMENT_ROOT'].'/php/page-head.php' ?>
 			<div id="main">
-				<h2>List of All Mission Types</h2>
+				<h2 class="list-header">List of All Mission Types</h2>
 				<p><a href="/mission/mission-type.php">Add new Mission Type</a></p>
 				<?php listMissionType(); ?>
 			</div>
