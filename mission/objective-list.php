181da2af362d390ddff90877b56f1691d6601313
Josh Chambers <joshua.e.chambers@gmail.com>
2021-11-15 15:49:08 -0500

    Some objective updates

--- mission/objective-list.php
+++ mission/objective-list.php
@@ -1,14 +1,18 @@
-<?php include_once '/home/joshch9/project/adminInclude.php' ?>
+<?php
+use XCOMDatabank\Missions\Objective;
+
+include_once __DIR__.'../../project/adminInclude.php'; ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<?php include_once $_SERVER['DOCUMENT_ROOT'].'/php/header-include.php' ?>
-<?php include_once $_SERVER['DOCUMENT_ROOT'].'/php/page-head.php' ?>
+    <?php include_once __DIR__.'/../php/header-include.php' ?>
+    <body>
+        <?php include_once __DIR__.'/../php/page-head.php' ?>
 			<div id="main">
 				<h2 class="list-header">List of All Mission Objectives</h2>
-				<p><a href="/mission/objective.php">Add new Mission Objective</a></p>
-				<?php listObjective(); ?>
+				<p><a href="objective.php">Add new Mission Objective</a></p>
+                <?php Objective::getListPage(); ?>
 			</div>
-	<?php include_once $_SERVER['DOCUMENT_ROOT'].'/php/scripts-include.php' ?>
+        <?php include_once __DIR__.'/../php/scripts-include.php' ?>
 	</body>
 </html>
--- mission/objective.php
+++ mission/objective.php
@@ -1,44 +1,51 @@
-<?php include_once '/home/joshch9/project/adminInclude.php' ?>
 <?php
+use XCOMDatabank\Missions\Objective;
 
-	$objective = new Objective();
+include_once __DIR__.'../../project/adminInclude.php';
+
+$errorMsg = "";
+$objective = new Objective();
 	
-	if(!empty($_POST)) {
-		$objectiveData = $_POST;
-		
-		if(isset($_POST['id'])) {
-			if(is_numeric($_POST['id'])) {
-				$objective->editObjective($objectiveData);
-			}
-		}
-		else {
-			$objective->newObjective($objectiveData);
-		}
-		createMissionJson();
-		header('Location: /mission/objective-list.php');
-	}
-	else {
-		if(isset($_GET['id']) and is_numeric($_GET['id'])) {
-			$objectiveID = $_GET['id'];
-			$objective->getObjective($objectiveID);
-		} ?>
+if(!empty($_POST)) {
+    $errorMsg = $objective->processForm($_POST, '/mission/objective-list.php');
+}
+else {
+    if(isset($_GET['id']) and is_numeric($_GET['id'])) {
+        $objectiveID = $_GET['id'];
+        $objective->getObjective($objectiveID);
+    }
+    ?>
 
 <!DOCTYPE html>
-<html lang="en">
-<?php include_once $_SERVER['DOCUMENT_ROOT'].'/php/header-include.php' ?>
-<?php include_once $_SERVER['DOCUMENT_ROOT'].'/php/page-head.php' ?>
+    <html lang="en">
+    <?php include_once __DIR__.'/../php/header-include.php' ?>
+    <body>
+        <?php include_once __DIR__.'/../php/page-head.php' ?>
 			<div id="main" class="controls input-group">
-				<form action="objective.php" method="post" id="objective-form">
-					<?php objectiveForm($objective); ?>
-					
-					<input type="submit" value="Submit" class="submit"> <input type="reset">
+                <h2 class="list-header">Add/Edit Mission Type</h2>
+                <?php
+                if($errorMsg != "") {
+                    ?>
+                    <p class="text-danger"><strong><?php echo $errorMsg; ?></strong></p>
+                    <?php
+                }
+                ?>
+				<form action="objective.php" method="post" id="objective-form" enctype="multipart/form-data" class="was-validated" novalidate>
+                    <div class="g-3 row">
+                        <?php Objective::getObjectiveForm($objective); ?>
+                    </div>
+
+                    <div class="g-3 row">
+                        <div class="col-12">
+                            <button type="submit" id="submit" class="submit btn btn-primary">Submit</button>
+                        </div>
+                    </div>
 					
 				</form>
 			</div>
-	<?php include_once $_SERVER['DOCUMENT_ROOT'].'/php/scripts-include.php' ?>
+        <?php include_once __DIR__.'/../php/scripts-include.php' ?>
 	</body>
 </html>
 
 <?php		
-	}	
-?>
+	}
