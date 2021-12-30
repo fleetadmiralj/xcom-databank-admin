a3c49c8d18e5059d24b553eb285e85d69a1cba93
Josh Chambers <joshua.e.chambers@gmail.com>
2021-12-29 21:01:03 -0500

    Update missions.php

--- json/missions.php
+++ json/missions.php
@@ -1,33 +1,29 @@
-<?
+<?php
 
-	include_once '/home/joshch9/project/mainInclude.php';
-		
-	$dbh = openDatabase();
-		
-	$stmt = $dbh->prepare("SELECT * FROM xcom_mission_type ORDER BY description");
-	$stmt -> execute();
-		
-	$missionArray = [];
-		
-	while ($row = $stmt->fetch()) {
-			
-		$missionArray[$row['description']] = [];
-			
-		$stmt2 = $dbh->prepare('SELECT id, description FROM xcom_objective WHERE mission_type_id = :id');
-		$stmt2->bindParam(':id', $row['id']);
-		$stmt2 -> execute();
-			
-		while($row2 = $stmt2->fetch()) {
-			$missionArray[$row['description']][$row2['id']] = $row2['description'];
-		}
-			
-		$stmt2 = null;
-	}
+include_once __DIR__.'/../../project/adminInclude.php';
 
-	header('Content-Type: application/json');
-	echo json_encode($missionArray);
-		
-	$dbh = null;
-	$stmt = null;
+use XCOMDatabank\Utility\Database;
 
-?>
+$query = "SELECT * FROM xcom_mission_type WHERE enabled = true ORDER BY description";
+$params = array();
+
+$queryResult = Database::runQuery('select', $query, $params);
+		
+$missionArray = [];
+		
+while ($row = $queryResult->fetch()) {
+			
+    $missionArray[$row['description']] = [];
+
+    $query = 'SELECT id, description FROM xcom_objective WHERE mission_type_id = :id AND enabled = true';
+    $params[0] = array("param" => ":id", "var" => $row['id'], "type" => PDO::PARAM_INT,);
+
+    $queryResult2 = Database::runQuery('select', $query, $params);
+			
+    while($row2 = $queryResult2->fetch()) {
+        $missionArray[$row['description']][$row2['id']] = $row2['description'];
+    }
+}
+
+header('Content-Type: application/json');
+echo json_encode($missionArray);
