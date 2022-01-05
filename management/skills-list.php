046bfea57969d94bdf6f7eeb4a62c93f1e3952dd
Joshua Chambers <joshua.e.chambers@gmail.com>
2022-01-04 21:35:45 -0500

    Update style.css and skills-list.php

--- management/skills-list.php
+++ management/skills-list.php
@@ -9,7 +9,7 @@ include_once __DIR__.'/../../project/adminInclude.php'; ?>
 <body>
 <?php include_once __DIR__.'/../php/page-head.php' ?>
 <div id="main">
-    <h2 class="list-header">List of All Skill</h2>
+    <h2 class="list-header">List of All Skills</h2>
     <p><a href="skills.php">Add new Skill</a></p>
     <?php Skill::getListPage(); ?>
 </div>
