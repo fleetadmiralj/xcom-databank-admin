5c8235012c991eb712c1178f70ddbd94aa24761a
Joshua Chambers <joshua.e.chambers@gmail.com>
2022-01-11 20:06:24 -0500

    Update mission-alien-list.php

--- mission/mission-alien-list.php
+++ mission/mission-alien-list.php
@@ -10,7 +10,7 @@ include_once __DIR__.'/../../project/adminInclude.php'; ?>
 <?php include_once __DIR__.'/../php/page-head.php' ?>
 <div id="main">
     <h2 class="list-header">List of All Mission Aliens</h2>
-    <p><a href="mission-soldier.php">Add new Mission Alien</a></p>
+    <p><a href="mission-alien.php">Add new Mission Alien</a></p>
     <?php MissionAlien::getListPage(); ?>
 </div>
 <?php include_once __DIR__.'/../php/scripts-include.php' ?>
